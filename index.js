1e46ad83ca8db53d3694215d71663b800de22fa2
Thomas Taylor <thomas.taylor@kineo.com>
2019-04-16 21:29:34 +0100

    Remove old file references

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-const { Singleton, Utils } = require('adapt-authoring-core');
 const Logger = require('./lib/logger');
-module.exports = Utils.compose(Logger, Singleton);
+
+const instance = new Logger();
+module.exports = instance;
