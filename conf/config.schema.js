1916e2d5a93d69378206e320f7f668d898185255
Thomas Taylor <thomas.taylor@kineo.com>
2019-07-12 14:32:00 +0100

    Add config schema

---
+++ conf/config.schema.js
@@ -0,0 +1,12 @@
+module.exports = {
+  definition: {
+    enabledLevels: {
+      type: 'Array',
+      default: ['error', 'warn', 'success', 'info', 'debug']
+    },
+    showTimestamp: {
+      type: 'Boolean',
+      default: true
+    }
+  }
+};
